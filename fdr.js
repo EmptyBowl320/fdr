18344d14b3a2d8a09a2a3c4168870043b65ecb7f
EmptyBowl320 <40175855+EmptyBowl320@users.noreply.github.com>
2018-06-21 18:52:35 -0500

    Add files via upload

--- fdr.js
+++ fdr.js
@@ -3,6 +3,7 @@ window.onload = function (){
 document.querySelector('#title-text').onmouseover = h1SecretMessage;
 document.querySelector('#title-text').onmouseout = h1NonSecretMessage;
 document.querySelector('.generalSecretMessage').onmouseover = h1SecretMessage;
+document.querySelector('.generalSecretMessage').onmouseover = GenralSecretMessage;
 document.querySelector('.generalSecretMessage').onmouseout = GenralNonSecretMessage;
 
 
@@ -20,6 +21,14 @@ function h1NonSecretMessage(){
     this.style.color = 'black'
 }
 
+function GenralSecretMessage(){
+    this.innerHTML = "STALIN IS PEACE"
+    this.style.color = "#d20f2e";
+    this.style.background ="#7d7a77"
+}
+
 function GenralNonSecretMessage(){
-    this.innerHTML = General Information
+    this.innerHTML = "General Information"
+    this.style.background = "#8da1b9"
+    this.style.color = 'black'
 }
